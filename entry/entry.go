5deadced510ad71eaa9558ed59915fb1d0b77a78
reber0 <reber0ask@qq.com>
2022-10-01 00:06:10 +0800

    解决不能通过 b 站 api 获取信息的问题

--- entry/entry.go
+++ entry/entry.go
@@ -2,7 +2,7 @@
  * @Author: reber
  * @Mail: reber0ask@qq.com
  * @Date: 2022-01-18 09:23:30
- * @LastEditTime: 2022-09-20 10:38:27
+ * @LastEditTime: 2022-09-30 23:56:29
  */
 package entry
 
@@ -32,4 +32,5 @@ func AppInit() {
 	global.Client.SetTimeout(time.Duration(20) * time.Second)
 	global.Client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
 	global.Client.SetHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:78.0) Gecko/20100101 Firefox/78.0")
+	global.Client.SetHeader("Cookie", "a=b") // b 站 api 获取需要带 cookie
 }
