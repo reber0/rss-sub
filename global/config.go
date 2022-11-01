3efb681212a5c09e90d336de911d56524173a925
reber0 <reber0ask@qq.com>
2022-11-01 17:07:27 +0800

    update port

--- global/config.go
+++ global/config.go
@@ -2,11 +2,11 @@
  * @Author: reber
  * @Mail: reber0ask@qq.com
  * @Date: 2022-11-01 15:23:49
- * @LastEditTime: 2022-11-01 15:24:16
+ * @LastEditTime: 2022-11-01 17:07:19
  */
 package global
 
 var (
 	ListenIP   = "127.0.0.1"
-	ListenPort = 8082
+	ListenPort = 8081
 )
