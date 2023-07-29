c696366e2e815dd6fb77b447a3b5345c4e713a1c
reber0 <reber0ask@qq.com>
2023-07-29 12:06:57 +0800

    update

--- schedule/schedule.go
+++ schedule/schedule.go
@@ -2,7 +2,7 @@
  * @Author: reber
  * @Mail: reber0ask@qq.com
  * @Date: 2022-02-04 23:33:28
- * @LastEditTime: 2023-06-04 17:34:24
+ * @LastEditTime: 2023-07-29 12:06:48
  */
 package schedule
 
@@ -18,7 +18,7 @@ func Start() {
 	specArticle := fmt.Sprintf("0 0 */%d * * *", 6) // 整点执行，6/12/18/24 点执行
 	crontab.AddFunc(specArticle, checkArticle)
 
-	specVideo := fmt.Sprintf("0 */%d * * * *", 59) // 整点执行，每 30 分钟执行一次
+	specVideo := fmt.Sprintf("0 */%d * * * *", 59) // 整点执行，每 59 分钟执行一次
 	crontab.AddFunc(specVideo, checkVideo)
 
 	crontab.Start()
